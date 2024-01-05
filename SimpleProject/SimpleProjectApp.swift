1a049dbf7277395d20cc2c49d3d8d1bf988ce227
Arnold Noronha <arnold@screenshotbot.io>
2024-01-05 10:56:16 -0500

    Initial Commit

---
+++ SimpleProject/SimpleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleProjectApp.swift
+//  SimpleProject
+//
+//  Created by Arnold Noronha on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
