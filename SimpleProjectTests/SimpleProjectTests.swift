e9a6fdb0051307ad6a73da70cbf6781201419b38
Arnold Noronha <arnold@screenshotbot.io>
2024-02-20 15:19:51 -0500

    ...

--- SimpleProjectTests/SimpleProjectTests.swift
+++ SimpleProjectTests/SimpleProjectTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import SnapshotTesting
+
 @testable import SimpleProject
 
 final class SimpleProjectTests: XCTestCase {
@@ -25,6 +26,8 @@ final class SimpleProjectTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let view = ContentView()
+        assertSnapshot(matching: view, as: .image)
     }
 
     func testPerformanceExample() throws {
