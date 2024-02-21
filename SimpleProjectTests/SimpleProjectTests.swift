fae5193097b28915e4662f055a307616f935c86e
Arnold Noronha <arnold@screenshotbot.io>
2024-02-20 19:50:52 -0500

    Remove performance test

--- SimpleProjectTests/SimpleProjectTests.swift
+++ SimpleProjectTests/SimpleProjectTests.swift
@@ -32,9 +32,6 @@ final class SimpleProjectTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
