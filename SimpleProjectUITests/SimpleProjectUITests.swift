abffcc04c966244ab9a34ea7dde1d0bb9b18ae1b
Arnold Noronha <arnold@tdrhq.com>
2024-02-22 23:14:57 -0500

    Disable performance test

--- SimpleProjectUITests/SimpleProjectUITests.swift
+++ SimpleProjectUITests/SimpleProjectUITests.swift
@@ -31,11 +31,11 @@ final class SimpleProjectUITests: XCTestCase {
     }
 
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        // if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+        //    // This measures how long it takes to launch your application.
+        //    measure(metrics: [XCTApplicationLaunchMetric()]) {
+        //        XCUIApplication().launch()
+        //    }
+        //}
     }
 }
