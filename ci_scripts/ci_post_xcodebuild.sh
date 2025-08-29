bc9a2051cbefc6276cb3b3736e0c63fa3b979e6d
Arnold Noronha <arnold@screenshotbot.io>
2025-08-29 16:43:35 -0400

    ..

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -13,5 +13,5 @@ cd ..
 # Only run after successful test actions
 if [[ "$CI_XCODEBUILD_ACTION" == "test-without-building" ]] && [[ -d "$CI_RESULT_BUNDLE_PATH" ]]; then
     curl https://screenshotbot.io/recorder.sh | sh
-    ~/screenshotbot/recorder --xcresult $CI_RESULT_BUNDLE_PATH --channel swift-snapshot-testing-example-xcode-cloud --commit-limit 0
+    ~/screenshotbot/recorder --xcresult $CI_RESULT_BUNDLE_PATH --channel swift-snapshot-testing-example-xcode-cloud
 fi
