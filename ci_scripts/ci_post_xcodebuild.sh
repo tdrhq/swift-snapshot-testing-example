ed09daa2e7fb615c5535bb936c5c3cd92a8fd9c4
Arnold Noronha <arnold@screenshotbot.io>
2025-08-28 23:44:13 -0400

    Only run after test

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+echo "CI_XCODEBUILD_ACTION: $CI_XCODEBUILD_ACTION"
 
-curl https://screenshotbot.io/recorder.sh | sh
-~/screenshotbot/recorder --xcresult $CI_RESULT_BUNDLE_PATH --channel swift-snapshot-testing-example-xcode-cloud --main-branch main
+# Only run after successful test actions
+if [[ "$CI_XCODEBUILD_ACTION" == "test-without-building" ]] && [[ -d "$CI_RESULT_BUNDLE_PATH" ]]; then
+    curl https://screenshotbot.io/recorder.sh | sh
+    ~/screenshotbot/recorder --xcresult $CI_RESULT_BUNDLE_PATH --channel swift-snapshot-testing-example-xcode-cloud --main-branch main
+fi
