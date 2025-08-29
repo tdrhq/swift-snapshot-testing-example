42b70f261c0ee113cfbb755d020caf7365897a13
Arnold Noronha <arnold@screenshotbot.io>
2025-08-28 23:18:08 -0400

    Singular

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,4 +4,4 @@ set -e
 
 
 curl https://screenshotbot.io/recorder.sh | sh
-~/screenshotbot/recorder --xcresults $CI_RESULT_BUNDLE_PATH --channel swift-snapshot-testing-example-xcode-cloud
+~/screenshotbot/recorder --xcresult $CI_RESULT_BUNDLE_PATH --channel swift-snapshot-testing-example-xcode-cloud
