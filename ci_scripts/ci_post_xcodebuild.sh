059571ea29a7f46b7e25ff570f9d8338d685d963
Arnold Noronha <arnold@screenshotbot.io>
2025-08-28 23:54:27 -0400

    Change directory

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -2,7 +2,13 @@
 
 set -e
 
+echo "Current directory: $(pwd)"
 echo "CI_XCODEBUILD_ACTION: $CI_XCODEBUILD_ACTION"
+echo "Environment variables:"
+env
+
+# Change to project root
+cd $CI_WORKSPACE
 
 # Only run after successful test actions
 if [[ "$CI_XCODEBUILD_ACTION" == "test-without-building" ]] && [[ -d "$CI_RESULT_BUNDLE_PATH" ]]; then
